ba6955bf12718e5981fd26326616010bbcba2e25
todigala, sreedevi (st681u) <st681u@att.com>
2020-02-03 20:04:11 -0600

    this is commit from GitStuff

--- searchdetails_tumbs_up_tumbs_down_01.js
+++ searchdetails_tumbs_up_tumbs_down_01.js
@@ -28,6 +28,10 @@ describe('searchdetails_tumbs_up_tumbs_down_01.js- Thumbs up and Thumbs down val
 	});
 	it('Select Column Search Validate thumbsUp and thumbsDown icons', function () {
 		console.log("***********************  searchdetails_tumbs_up_tumbs_down_01.js ***********************************");
+		console.log("***********************  searchdetails_tumbs_up_tumbs_down_02.js ***********************************");
+		console.log("***********************  searchdetails_tumbs_up_tumbs_down_03.js ***********************************");
+		console.log("***********************  searchdetails_tumbs_up_tumbs_down_04.js ***********************************");
+		console.log("***********************  searchdetails_tumbs_up_tumbs_down_05.js ***********************************");
 
 		searchResultsAllDataClasses.searchCheckuncheckAll();
 		searchResultsApplication.applicationSearch('customer');
